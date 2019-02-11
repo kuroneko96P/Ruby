074f1b9d718bba1bff6d00a30dcde48ed7940224
kuroneko96P <runowaaru96@gmail.com>
2019-02-11 21:11:47 +0900

    first commit

---
+++ variable.rb
@@ -0,0 +1,17 @@
+# 変数に値を格納
+name = "Satou"
+
+age = 33
+
+address = "Kanagawa"
+
+tell = "080-1234-5678"
+
+
+# 変数に格納したデータを式展開で表示
+puts "【自己紹介】"
+puts "私の名前は#{name}です。"
+puts "年齢は#{age}です。"
+puts "今住んでいる所は#{address}です。"
+puts "電話番号は#{tell}です。"
+puts "宜しくお願い致します。"
