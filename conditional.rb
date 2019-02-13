db52dcf163dff6590924e57fed262fb09541b836
kuroneko96P <runowaaru96@gmail.com>
2019-02-13 21:38:10 +0900

    first commit

---
+++ conditional.rb
@@ -0,0 +1,27 @@
+# ifを使用した戦闘ゲーム
+
+# 勇者のHP
+brave_hp = 30
+# 勇者の攻撃力
+brave_attack = 10
+# 勇者の防御力
+brave_defense = 5
+# 敵のhp
+enemy_hp = 30
+# 敵の攻撃力
+enemy_attack = 5
+# 敵の防御力
+enemy_defense = 10
+
+# 勇者・敵のステータス
+
+brave_hp =30
+brave_attack = 10
+brave_defence = 5
+
+enemy_hp = 30
+enemy_attack = 5
+enemy_defense = 10
+
+# 敵に与えるダメージの計算
+enemy_defense = brave_attack - enemy_defense
